fc38f126d9db8a15ef5b54dcf4a3a361e41548c3
Gokulkiran418 <umgokulkiran@gmail.com>
2025-07-05 01:40:04 +0530

    Add navbar home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import About from './components/About';
 import Services from './components/Services';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Services />
       <About />
